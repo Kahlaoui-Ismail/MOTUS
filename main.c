35ea43c7157a8c232d781d4598691f8bce56d724
Kahlaoui-Ismail <55671069+Kahlaoui-Ismail@users.noreply.github.com>
2019-12-04 13:16:35 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+typedef struct Couleur{
+    char couleur[7];
+    //JAUNE,BLUE,ROUGE
+}Clr;
+
+
+typedef struct Case{
+    char val;
+    Couleur c;
+}casemot;
+
+
+
+void Aleatoire_Lecture_Dicto(){
+    //Lecture fichier .txt
+    
+    return String;
+}
+
+
+void  Minuteur(){
+    time.h
+};
+
+void saisir_mot(){
+    char mot[8];
+    char ascii(ENTER);
+    //Vérifier la taille du mot avec l'utilisateur.
+}
+
+
+void Matrix(){
+    casemot M[7][8];
+    
+    //
+}
+
+void check(){
+    //prend 1 lettre saisie et le mot aléatoire
+    Attribuer JAUNE SI mal-placée
+    Attribuer ROUGE SI CORRECTE
+    Attribuer BLUE SI non existants
+}
+
+void score(){
+    si(verifier_existance_dicto()==true){
+        pénalisé.
+    };
+    
+    voir image.
+}
+
+
+boolean verifier_existance_dicto(){
+    //parcourir le dictionnaire et vérifier l'existance du mot proposé
+    Non existant = false;
+    existant = true;
+}
+
+void MENU(){
+    // Nouvellepartie(); Initialisation du partie.
+    Minuteur();
+    
+    int i=0;
+    while(i<7 && motvalide(saisir_mot())){
+        
+        saisir_mot();
+        
+        check(Aleatoire_Lecture_Dicto(),saisir_mot());
+        Actualiser score();
+            }
+    i++;
+    
+}
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
