fc115b6b47997686402dbc68be47787faae00741
hsouf <37840702+hsouf@users.noreply.github.com>
2019-12-04 21:07:15 +0100

    Update main.c

--- main.c
+++ main.c
@@ -12,11 +12,42 @@ typedef struct Case{
 }casemot;
 
 
-
+/* random int between 0 and N : 
+ int r = rand() % N;              */
 void Aleatoire_Lecture_Dicto(){
-    //Lecture fichier .txt
+   
+   FILE *filePointer ; 
+      
+    // Declare the variable for the data to be read from file 
+    char le_mot_aléatoire[50]; 
+  
     
-    return String;
+    filePointer = fopen("dictionnaire.txt", "r") ; 
+      
+   
+    if ( filePointer == NULL ) 
+    { 
+        printf( "dictionnaire.txt file failed to open." ) ; 
+    } 
+    else
+    { 
+          
+        printf("The file is now opened.\n") ; 
+          
+        // Read the dataToBeRead from the file 
+        //<!> fgets() permet de lire le fichier char par char....
+        while( fgets ( dataToBeRead, 50, filePointer ) != NULL ) 
+        { 
+          
+            // Print the dataToBeRead  
+            printf( "%s" , dataToBeRead ) ; 
+         } 
+          
+         
+        fclose(filePointer) ; 
+          
+    } 
+    return 0;   
 }
 
 
@@ -33,24 +64,44 @@ void saisir_mot(){
 
 void Matrix(){
     casemot M[7][8];
-    int n;
+    
     
     //
 }
 
-void check(){
+void initialiser_partie(){
+    
+}//pour commencer une nouvelle partie une fois le mot en question est trouvé
+
+void check_color_case(){
+     if(lettre_bien_placée()==true){   //Reste  à définir les fonctions suivantes: lettre_bien_placée , lettre_non_existante, mot_trouvé
+        couleur_lettre="rouge";
+    }else{
+        couleur_lettre="jaune";
+    }
+    if(lettre_non_existante()==true){
+        couleur_lettre="bleue";}
     //prend 1 lettre saisie et le mot aléatoire
-    Attribuer JAUNE SI mal-placée
-    Attribuer ROUGE SI CORRECTE
-    Attribuer BLUE SI non existants
+    //Attribuer JAUNE SI mal-placée
+    //Attribuer ROUGE SI CORRECTE
+    //Attribuer BLUE SI non existants
 }
 
 void score(){
-    si(verifier_existance_dicto()==true){
-        pénalisé.
-    };
-    
-    voir image.
+    int score_partie=0;
+    if(verifier_existance_dicto(char*mot_inseré)==false){
+        score_partie-=4;//(-4) for a non existent word
+      };
+    if(lettre_bien_placée()==true){
+        score_partie+=2;// !!!!!<?> pour le tableau des scores chaque niveau de difficulté a sa propre notation donc on optera pour la notation du niv facile....
+    }else{
+        score_partie+=1;
+    }
+    if(mot_trouvé()==true){
+        score_partie+=6;
+    }
+    if(mot_déjà_proposé()==true){
+        score_partie-=10;
 }
 
 
