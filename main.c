d4b4e0c023b8377ab6b639bf51367e812924a4f0
Kahlaoui-Ismail <55671069+Kahlaoui-Ismail@users.noreply.github.com>
2019-12-07 22:47:54 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,9 @@
 #include <string.h>
 #include <ctype.h>
 
-typedef enum {true, false} bool;
+#define ENTER 13
+
+typedef enum {true, false} bool; // On va définir le type booléan pour faciliter la manipulation.
 
 typedef struct Couleur{
     char couleur[7];
@@ -16,19 +18,7 @@ typedef struct Case{
     Couleur c;
 }casemot;
 
-bool validate(char *mot){
-    if(strlen(mot)!=9){
-        return false;
-    }
-    else{
-        for(int i=0;i<strlen(mot)-2;i++){
-            if(isalpha(mot[i])==0){
-                return false;
-            }
-        }
-    }
-    return true;
-}
+
 
 /* random int between 0 and N : 
  int r = rand() % N;              */
@@ -51,39 +41,40 @@ void Aleatoire_Lecture_Dicto(){
     return 0;   
           };
 
-bool validate(char *mot){
-    if(strlen(mot)!=9){
-        return false;
-        }
-    else{
-        for(int i=0;i<strlen(mot)-2;i++){
-            if(isalpha(mot[i])==0){
-                return false;
-            }
-        }
-    }
-        return true;
-}
+
           
 void  Minuteur(){
     time.h
 };
 
+bool validate(char *mot){  //cette fonction vérifie le mot saisi dans la grille
+    if(strlen(mot)!=9){    //Doit contenir 8 caractères alphabétiques et le caractère ENTER
+        return false;
+    }
+    else{
+        for(int i=0;i<strlen(mot)-2;i++){ // On vérifie que le 8 caractères sont alphabétique(la validation du caractère     ENTER
+            if(isalpha(mot[i])==0){       // ENTER se fait dans la fonction saisir_mot()
+                return false;
+            }
+        }
+    }
+    return true; //renvoie TRUE si seulement si les 8 caractères sont alphabétiques
+}
           
 char *saisir_mot(){
     char *mot ;
     int enter;
     mot=(char *)malloc(sizeof(char));
-    //char ascii(ENTER);
+
         
 do{
         
         printf("Faites saisir un mot valide");
         scanf("%s",mot);
         enter=mot[8];
-        }while(enter!=13 || validate(mot)==false); //Ne sortir de la boucle que si le mot tapée est valide
-          return mot;
-}
+        }while(enter!=ENTER || validate(mot)==false); //Ne sortir de la boucle que si le mot tapée est valide et que 
+          return mot;                                 //le caractère saisie dans la position 8 est bien le caractère
+}                                                     // ENTER dont le code ASCII est 13
 
 
 void Matrice_mot(){  // il faut définir une matrice initiale où les cases ont une couleur NULLE
