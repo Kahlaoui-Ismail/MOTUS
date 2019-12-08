46d1844515819cf6abb4b1ddb49985a53a8318b9
hsouf <37840702+hsouf@users.noreply.github.com>
2019-12-08 10:08:21 +0100

    Update main.c
    MINUTEUR PROP

--- main.c
+++ main.c
@@ -51,6 +51,37 @@ void Minuteur(int seconds){ // le temps de réflexion en seconds
     while(clock()<start+mseconds)
   ;
 }
+/*   AUTRE PROPOSITION POUR LA FONCTION MINUTEUR=> AFFICHE LE NOMBRE DE SECONDES RESTANTES 
+VOID MINUTEUR (){
+	unsigned int x_hours=0;
+	unsigned int x_minutes=0;
+	unsigned int x_seconds=0;
+	unsigned int x_milliseconds=0;
+	unsigned int totaltime=0,count_down_time_in_secs=0,time_left=0;
+
+	clock_t x_startTime,x_countTime;
+	count_down_time_in_secs=20;  // 1 minute is 60, 1 hour is 3600
+
+
+    x_startTime=clock();  // start clock
+    time_left=count_down_time_in_secs-x_seconds;   // update timer
+
+	while (time_left>0)
+	{
+		x_countTime=clock(); // update timer difference
+		x_milliseconds=x_countTime-x_startTime;
+		x_seconds=(x_milliseconds/(CLOCKS_PER_SEC))-(x_minutes*60);
+		x_minutes=(x_milliseconds/(CLOCKS_PER_SEC))/60;
+		x_hours=x_minutes/60;
+        time_left=count_down_time_in_secs-x_seconds; // subtract to get difference
+        printf( "\nYou have %d seconds left ( %d )",time_left,count_down_time_in_secs);
+	}
+  
+  printf( "\n\n\nTime's out\n\n\n");
+  return 0;
+}
+*/
+
 
 bool validate(char *mot){  //cette fonction vérifie le mot saisi dans la grille
     if(strlen(mot)!=9){    //Doit contenir 8 caractères alphabétiques et le caractère ENTER
