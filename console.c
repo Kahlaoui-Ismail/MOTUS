94952fe7ba7e20c45b64f6abde1ce1e249e83c8d
hsouf <37840702+hsouf@users.noreply.github.com>
2020-01-16 10:40:41 +0100

    Update console.c

--- console.c
+++ console.c
@@ -3,39 +3,50 @@
 
 int best_score(){ //cherche dans un fichier le meilleur score enregistré dans les parties précédentes
   FILE*fscore;
-  fscore=fopen("scores.txt","r");
-  max=0
-  while(!feof(fptr)){
-  fscanf(fptr,"%s", &num);
+  fscore=fopen("‎⁨/Users/soufianehajazi/Desktop/TP2/TP-structures.c/score.txt","r");
+  int max=0;
+  int num;
+  while(!feof(fscore)){
+  fscanf(fscore,"%d", &num);
   
   if(max<num){
     max=num;
   }
+}
+return max;
+
 
 }
 
+
   void start_game(){
+    int x;
     
-    printf("PLEASE CHOOSE THE NUMBER OF WORDS");
-    printf("1) 6letters       2) 7letters     3) 8letters");
+    printf("\nPLEASE CHOOSE THE NUMBER OF WORDS\n");
+    printf("\n 1) 6letters       2) 7letters     3) 8letters\n");
     scanf("%d",&x);
     
 
-
+}
 
 void menu(){
 
   printf("\n********************WELCOME TO MOTUS******************\n\n");
   printf("\n              1- NEW GAME                  \n ");
   printf("\n              2- BEST SCORE                \n");
-  printf("\n              2- QUIT THE GAME               \n\n ");
+  printf("\n              3- QUIT THE GAME               \n\n ");
   printf("\n******************************************************\n");
   
 }
 
 
 int main(){
+  int n;
+  
+    
+
   menu();
+ 
   printf("enter your choice:");
   scanf("%d",&n);
   switch(n){
@@ -43,8 +54,8 @@ int main(){
     printf("**********************WELCOME TO MOTUS*******************");
       start_game();break;
     
-    case 2:
-    printf("Your best score is:%d", bestscore());break;
+    case 2: printf("ur best score: %d",best_score());
+   break;
     case 3:break;
     }
   return 0;
